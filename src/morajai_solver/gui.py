f45b2b3cb93b5b10791cc31c23cdd6a0357a9a40
Paul Talbot <15139535+pvtalbot@users.noreply.github.com>
2026-07-08 12:00:49 +0200

    remove logic from gui

--- src/morajai_solver/gui.py
+++ src/morajai_solver/gui.py
@@ -1,7 +1,4 @@
-import argparse
-
 import customtkinter as ctk
-import logging
 
 from morajai_solver.views.BoardView import BoardView
 from morajai_solver.views.ControlPanelView import ControlPanelView
@@ -16,31 +13,7 @@ def fade_out(app, alpha=1.0):
     else:
         app.destroy()
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-q', '--quiet',
-        action='store_true',
-        default=False,
-        help="Réduit le niveau de logs"
-    )
-    parser.add_argument(
-        '-v', '--verbose',
-        action='store_true',
-        default=False,
-        help="Augmente le niveau de logs"
-    )
-
-    args = parser.parse_args()
-    if args.quiet:
-        configure_logging(logger_level=logging.WARNING)
-    elif args.verbose:
-        configure_logging(logger_level=logging.DEBUG)
-    else:
-        configure_logging(logger_level=logging.INFO)
-
-    engine = GameEngine()
-
+def launch_gui():
     ctk.set_appearance_mode('dark')
     ctk.set_default_color_theme('blue')
 
@@ -77,6 +50,3 @@ def main():
 
 
     app.mainloop()
-
-if __name__ == '__main__':
-    main()
---
+++ src/morajai_solver/main.py
@@ -0,0 +1,33 @@
+import argparse, logging
+from  morajai_solver.logger import configure_logging
+from morajai_solver.core.game_engine import GameEngine
+from morajai_solver.gui import launch_gui
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-q', '--quiet',
+        action='store_true',
+        default=False,
+        help="Réduit le niveau de logs"
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        help="Augmente le niveau de logs"
+    )
+
+    args = parser.parse_args()
+    if args.quiet:
+        configure_logging(logger_level=logging.WARNING)
+    elif args.verbose:
+        configure_logging(logger_level=logging.DEBUG)
+    else:
+        configure_logging(logger_level=logging.INFO)
+
+    GameEngine()
+    launch_gui()
+
+if __name__ == '__main__':
+    main()
