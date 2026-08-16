2f62618fa547df27e5d3bc5fe87ec7b062fd529e
Paul Talbot <15139535+pvtalbot@users.noreply.github.com>
2026-08-16 17:24:00 +0200

    feat: move logbox to its own component file

--- src/morajai_solver/ui/panels/control_panel.py
+++ src/morajai_solver/ui/panels/control_panel.py
@@ -11,6 +11,7 @@ from morajai_solver.infra.events import (
     StepUpdatedEvent,
     VictoryAchievedEvent,
 )
+from morajai_solver.ui.components.log_box import LogBox
 from morajai_solver.ui.components.solution_display import SolutionDisplay
 from morajai_solver.ui.components.step_navigation_bar import StepNavigationBar
 from morajai_solver.ui.ui_colors import UITheme
@@ -25,9 +26,8 @@ class ControlPanel(ctk.CTkFrame):
 
         self._setup_ui()
 
-        self._append_log("Application démarrée.")
-        self._append_log("Prêt à résoudre...")
-        self.log_box.configure(state="disabled")
+        self.log_box.append_log("Application démarrée.")
+        self.log_box.append_log("Prêt à résoudre...")
 
         self.dispatcher.subscribe(StepUpdatedEvent, self._on_step_updated)
         self.dispatcher.subscribe(MoveEvaluatedEvent, self._on_move_evaluated)
@@ -54,35 +54,23 @@ class ControlPanel(ctk.CTkFrame):
         self.nav_bar = StepNavigationBar(self, ui_bus=self.dispatcher)
         self.nav_bar.grid(row=1, column=0, sticky="ew", padx=10, pady=5)
 
-        self.log_box = ctk.CTkTextbox(
-            self,
-            height=130,
-            fg_color=UITheme.BG_CONSOLE.value,
-            text_color=UITheme.TEXT_CONSOLE.value,
-            font=("Courier New", 12),
-        )
+        self.log_box = LogBox(self)
         self.log_box.grid(row=2, column=0, pady=(5, 10), padx=10, sticky="nsew")
 
     # --- Click handlers & helpers ---
     def _on_solution_found(self, event: SolutionFoundEvent):
         if event.result is None:
-            self._append_log("Aucune solution possible")
+            self.log_box.append_log("Aucune solution possible")
         elif len(event.result) == 0:
-            self._append_log("La grille est déjà résolue !")
+            self.log_box.append_log("La grille est déjà résolue !")
         else:
-            self._append_log(f"Solution trouvée en {len(event.result)} coups")
+            self.log_box.append_log(f"Solution trouvée en {len(event.result)} coups")
 
         self.solution_display.display_solution(event.result)
 
-    def _append_log(self, message: str):
-        self.log_box.configure(state="normal")
-        self.log_box.insert("end", f"> {message}\n")
-        self.log_box.see("end")
-        self.log_box.configure(state="disabled")
-
     # --- Event handlers ---
     def _on_victory_achieved(self, _):
-        self._append_log("VICTOIRE !")
+        self.log_box.append_log("VICTOIRE !")
 
     def _on_solution_invalidated(self, _):
         self.solution_display.clear_solution()
